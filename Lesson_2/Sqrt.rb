e4f123d8d71f8da1e21e02c55cc6a73a1796b5fb
Sasha_Drevina <_@email.com>
2021-05-17 22:27:18 +0300

    first commit

--- Lesson_2/Sqrt.rb
+++ Lesson_2/Sqrt.rb
@@ -4,14 +4,15 @@ b = gets.chomp.to_i
 c = gets.chomp.to_i
 
 D = ((b**2) - (4*a*c))
+C = Math.sqrt(D).to_i
 
 if D > 0
-	x1 = (-b + Math.sqrt(D))
-	x2 = (-b - Math.sqrt(D))
+	x1 = ((-b + C)/2*a)
+	x2 = ((-b - C)/2*a)
 	puts "Дискриминант: #{D}, корни уравнения: #{x1} и #{x2}."
 elsif D == 0
 	x = -b/(2*a)
 	puts "Дискриминант: #{D}, корень уравнения: #{x}."
 else
 	puts "Дискриминант: #{D}. Корней нет."
-end
+end 
