70c32e0409036bd07f3ad0d2bee9a7ec6e5c64d0
Sanya <79960498+drevina@users.noreply.github.com>
2021-05-19 00:00:45 +0300

    first commit

---
+++ Lesson_3/hash_months.rb
@@ -0,0 +1,18 @@
+hash = {
+	"January" => 31,
+	"February" => 28,
+	"March" => 31,
+	"April" => 30,
+	"May" => 31,
+	"June" => 30,
+	"July" => 31,
+	"August" => 31,
+	"September" => 30,
+	"October" => 31,
+	"November" => 30,
+	"December" => 31
+}
+
+hash.each do |m, d|
+	puts m if d == 30
+end
