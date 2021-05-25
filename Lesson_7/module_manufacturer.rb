c3669ac09bbf5641b9beba4598c74aea7ecc924d
shunya <79960498+drevina@users.noreply.github.com>
2021-05-25 17:23:27 +0300

    first commits

---
+++ Lesson_7/module_manufacturer.rb
@@ -0,0 +1,13 @@
+module Manufacturer
+
+  attr_accessor :manufacturer
+
+  def set_manufacturer(name)
+    self.manufacturer = name
+  end
+
+  def display_manufacturer
+    manufacturer
+  end
+  
+end
